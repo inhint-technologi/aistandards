23566c84863099a8ede2aa1aa995afde89a80dde
Bent Harald Hansen <140284436+inhint-technologi@users.noreply.github.com>
2024-02-06 01:22:30 +0100

    Add files via upload

--- controllers/flowise.js
+++ controllers/flowise.js
@@ -1,1 +1,32 @@
+export const createPrediction = async (req, res) => {
+  const { message } = req.body;
+  console.log(message);
 
+  try {
+    // Call the Flowise API endpoint here..
+    const flowiseData = {
+      question: message,
+    };
+
+    // Call the flowise Endpoint
+    const response = await fetch(
+      `${process.env.FLOWISE_URL}/api/v1/prediction/${process.env.FLOW_ID}`,
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${process.env.FLOWISE_API_KEY}`,
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(flowiseData)
+      }
+    );
+    
+    const data = await response.json();
+    console.log(data);
+
+    res.status(200).json({ message: data.text });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+};
